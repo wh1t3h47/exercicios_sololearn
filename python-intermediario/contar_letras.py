70032f847c22964030893936b8b4a5fd6369577b
wh1t3h47 <tom.mharres@gmail.com>
2021-06-23 15:39:35 -0300

    python intermediario

---
+++ python-intermediario/contar_letras.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+
+def contar_letras(palavra):
+    split_text = tuple(palavra)
+    letter_count_dict = {}
+    #your code goes here
+    
+    for letter in split_text:
+        if (not letter in letter_count_dict):
+            letter_count_dict[letter] = 1;
+        else:
+            letter_count_dict[letter] += 1
+    
+    return letter_count_dict
+
