a00dbe796ba47c3bedc5757d1090043f61e6eb5b
Antônio Martos Harres <tom.mharres@gmail.com>
2022-01-19 01:01:07 -0300

    Create functions.go

---
+++ functions.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+    mine()
+}
+
+func mine() {
+   // outputs GO 3 times
+    fmt.Println("GO")
+    fmt.Println("GO")
+     fmt.Println("GO")
+    //fmn.Println("0")
+    
+}
+
